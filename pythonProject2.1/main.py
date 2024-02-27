a2b9206657c4cd2b0a368d96cbf76261c8282508
qiuzhijiejie <3075096335@qq.com>
2024-02-28 01:50:26 +0800

    基础语法知识

---
+++ pythonProject2.1/main.py
@@ -0,0 +1,34 @@
+'''1.
+number=100
+university="一流大学"'''
+
+'''2.
+a=b=c=100'''
+
+'''3.
+print("上辈子乱扔垃圾\n这辈子电子信息")'''
+
+'''4.
+float_num=13.14
+int_num=int(float_num)
+str_num=str(int_num)
+print("float_num")
+print("str_num")'''
+
+'''5.
+a=int(input("请输入一个整数:"))
+a=a//3
+print(a)'''
+
+'''6.
+print("厦门",end="");print('大学')'''
+
+'''7.
+name='林书凡'
+age=10
+print('%s的年龄是%d岁'% (name,age))'''
+
+'''8.
+name='林书豪'
+age = 10
+print(f'{name}的年龄是{age}岁')'''
